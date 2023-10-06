fb59ecb78866d40217796c465102d26e90bc3bb2
Azevedo <azevedorenan13@gmail.com>
2023-10-05 23:25:29 -0300

    Aplicacao de dados dinamicos e rotas dinamicas para o site utilizando routes

---
+++ Angular-Blog/src/app/data/dataFake.ts
@@ -0,0 +1,18 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo": "aaa",
+    "title": "saiu",
+    "description": "aiaiaiuiui",
+    "photoCover": ""
+
+  },
+  {
+    "id":"2",
+    "photo": "aaa",
+    "title": "saiu",
+    "description": "aiaiaiuiui",
+    "photoCover": ""
+
+  },
+]
